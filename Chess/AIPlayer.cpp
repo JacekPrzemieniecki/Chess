70005d72fa986a44337fc9453920b28384a7e7b4
jacek@przemieniecki.net <jacek@przemieniecki.net>
2013-11-14 00:51:27 +0100

    Add debug prints

--- Chess/AIPlayer.cpp
+++ Chess/AIPlayer.cpp
@@ -3,7 +3,6 @@
 #include "MoveValidator.h"
 #include <vector>
 #include <iostream>
-#include <thread>
 
 using namespace std;
 
@@ -18,13 +17,18 @@ AIPlayer::~AIPlayer()
 
 Move AIPlayer::MakeMove(Board& board)
 {
+	cout << "AI preparing to make move" << endl;
 	vector<Move*> possibleMoves;
 	GenerateAll(board, possibleMoves);
+	cout << "AI generated " << possibleMoves.size() << " moves" << endl;
 	Move chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
+	cout << "Choosing move from: " << chosenMove.from << " to " << chosenMove.to << endl;
 	int i = 0;
 	Board* boardAfterMove = new Board(&board, chosenMove);
+	cout << "Checking if king at position " << boardAfterMove->FindKing(false) << " is in check after AI move" << endl;
 	while (IsAttacked(*boardAfterMove, boardAfterMove->FindKing(false), false))
 	{
+		cout << "King in check, generating another move" << endl;
 		chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
 		boardAfterMove = new Board(&board, chosenMove);
 	}
