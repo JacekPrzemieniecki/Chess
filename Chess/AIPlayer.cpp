e45acfd48fb3fd002250a159ae73b199711d7e31
jacek@przemieniecki.net <jacek@przemieniecki.net>
2013-11-17 07:56:49 +0100

    run AI on a separate thread

--- Chess/AIPlayer.cpp
+++ Chess/AIPlayer.cpp
@@ -1,42 +1,39 @@
 #include "AIPlayer.h"
 #include "MoveGenerator.h"
 #include "MoveValidator.h"
+#include "Game.h"
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-AIPlayer::AIPlayer()
+namespace AIPlayer
 {
-}
 
-
-AIPlayer::~AIPlayer()
-{
-}
-
-Move AIPlayer::MakeMove(Board board)
-{
-	cout << "AI preparing to make move" << endl;
-	vector<Move*> possibleMoves;
-	GenerateAll(board, possibleMoves);
-	cout << "AI generated " << possibleMoves.size() << " moves" << endl;
-	Move chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
-	cout << "Choosing move from: " << chosenMove.from << " to " << chosenMove.to << endl;
-	int i = 0;
-	board.MakeMove(chosenMove);
-	cout << "Checking if king at position " << board.FindKing(false) << " is in check after AI move" << endl;
-	while (IsAttacked(board, board.FindKing(false), false))
+	void MakeMove(Game& game)
 	{
-		board.UndoMove();
-		cout << "King in check, generating another move" << endl;
-		chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
+		Board board = game.board;
+		cout << "AI preparing to make move" << endl;
+		vector<Move*> possibleMoves;
+		GenerateAll(board, possibleMoves);
+		cout << "AI generated " << possibleMoves.size() << " moves" << endl;
+		Move chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
+		cout << "Choosing move from: " << chosenMove.from << " to " << chosenMove.to << endl;
+		int i = 0;
 		board.MakeMove(chosenMove);
+		cout << "Checking if king at position " << board.FindKing(false) << " is in check after AI move" << endl;
+		while (IsAttacked(board, board.FindKing(false), false))
+		{
+			board.UndoMove();
+			cout << "King in check, generating another move" << endl;
+			chosenMove = *(possibleMoves[rand() % possibleMoves.size()]);
+			board.MakeMove(chosenMove);
+		}
+		board.UndoMove();
+		for (vector<Move*>::iterator it = possibleMoves.begin(); it != possibleMoves.end(); it++)
+		{
+			delete *it;
+		}
+		game.TryMakeMove(chosenMove);
 	}
-	board.UndoMove();
-	for (vector<Move*>::iterator it = possibleMoves.begin(); it != possibleMoves.end(); it++)
-	{
-		delete *it;
-	}
-	return chosenMove;
 }
